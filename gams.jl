48721347b05a5cd92937237a789b0dd755d821d2
slwu89 <10673535+slwu89@users.noreply.github.com>
2023-11-17 15:27:47 -0800

    use conjunctive query; same as data frame inner joins

--- gams.jl
+++ gams.jl
@@ -95,7 +95,7 @@ end
 # the acsets version
 
 using ACSets
-
+using Catlab # manipulate finite sets
 
 IJKLMSch = BasicSchema(
     [:I,:J,:K,:L,:M,:IJK,:JKL,:KLM], 
@@ -120,4 +120,145 @@ IJKLMSch = BasicSchema(
 
 @acset_type IJKLMData(IJKLMSch, index=[:IJK_I,:IJK_J,:IJK_K,:JKL_J,:JKL_K,:JKL_L,:KLM_K,:KLM_L,:KLM_M])
 
+n=8 # something large
+m=10 # 20
+
+# the basic sets
+I = collect(1:n)
+J = collect(1:m)
+K = collect(1:m)
+L = collect(1:m)
+M = collect(1:m)
+
+# make the data
 ijklm_dat = IJKLMData{Int}()
+
+add_parts!(ijklm_dat, :I, length(I))
+add_parts!(ijklm_dat, :J, length(J))
+add_parts!(ijklm_dat, :K, length(K))
+add_parts!(ijklm_dat, :L, length(L))
+add_parts!(ijklm_dat, :M, length(M))
+
+# add the relations...first as product...then sparsify
+
+# IJK
+ijk_prod = Iterators.product(I,J,K)
+
+add_parts!(
+    ijklm_dat, 
+    :IJK,
+    length(ijk_prod),
+    IJK_I=vec([e[1] for e in ijk_prod]),
+    IJK_J=vec([e[2] for e in ijk_prod]),
+    IJK_K=vec([e[3] for e in ijk_prod]),
+    value_ijk=SampleBinomialVec(I,J,K)
+)
+
+rem_parts!(
+    ijklm_dat, 
+    :IJK, 
+    findall(ijklm_dat[:,:value_ijk] .== 0)
+)
+
+# JKL
+jkl_prod = Iterators.product(J,K,L)
+
+add_parts!(
+    ijklm_dat, 
+    :JKL,
+    length(jkl_prod),
+    JKL_J=vec([e[1] for e in jkl_prod]),
+    JKL_K=vec([e[2] for e in jkl_prod]),
+    JKL_L=vec([e[3] for e in jkl_prod]),
+    value_jkl=SampleBinomialVec(J,K,L)
+)
+
+rem_parts!(
+    ijklm_dat, 
+    :JKL, 
+    findall(ijklm_dat[:,:value_jkl] .== 0)
+)
+
+# KLM
+klm_prod = Iterators.product(K,L,M)
+
+add_parts!(
+    ijklm_dat, 
+    :KLM,
+    length(klm_prod),
+    KLM_K=vec([e[1] for e in klm_prod]),
+    KLM_L=vec([e[2] for e in klm_prod]),
+    KLM_M=vec([e[3] for e in klm_prod]),
+    value_klm=SampleBinomialVec(K,L,M)
+)
+
+rem_parts!(
+    ijklm_dat, 
+    :KLM, 
+    findall(ijklm_dat[:,:value_klm] .== 0)
+)
+
+connected_paths_query = @relation (i=i,j=j,k=k,l=l,m=m) begin
+    IJK(IJK_I=i, IJK_J=j, IJK_K=k)
+    JKL(JKL_J=j, JKL_K=k, JKL_L=l)
+    KLM(KLM_K=k, KLM_L=l, KLM_M=m)
+end
+
+ijklm = query(ijklm_dat, connected_paths_query)
+
+
+
+# we want to check this against the DataFrames version
+
+IJK_sparse_df = DataFrame(tables(ijklm_dat).IJK)
+select!(IJK_sparse_df, Not(:value_ijk))
+rename!(IJK_sparse_df, [:i,:j,:k])
+
+JKL_sparse_df = DataFrame(tables(ijklm_dat).JKL)
+select!(JKL_sparse_df, Not(:value_jkl))
+rename!(JKL_sparse_df, [:j,:k,:l])
+
+KLM_sparse_df = DataFrame(tables(ijklm_dat).KLM)
+select!(KLM_sparse_df, Not(:value_klm))
+rename!(KLM_sparse_df, [:k,:l,:m])
+
+ijklm_df = DataFrames.innerjoin(
+    DataFrames.innerjoin(IJK_sparse_df, JKL_sparse_df; on = [:j, :k]),
+    KLM_sparse_df;
+    on = [:k, :l],
+)
+
+sort(ijklm) == sort(ijklm_df)
+
+
+
+
+# # remove redundant things in I,J,K,L,M
+
+# # find parts of `ob` that are not hit by any incoming homs and remove them
+# function not_in_image(acs, ob)
+#     in_homs = homs(acset_schema(acs), to=ob, just_names=true)    
+#     hit_by = [length.(incident(acs, parts(acs, ob), f)) for f in in_homs]
+#     to_rem = Int[]
+#     for i in parts(acs, ob)
+#         if all([h[i] for h in hit_by] .== 0)
+#             push!(to_rem, i)
+#         end
+#     end
+#     return to_rem
+# end
+
+# # for I, find things with no in-homs
+# not_in_image(ijklm_dat, :I)
+
+# # for J, find things with no in-homs
+# not_in_image(ijklm_dat, :J)
+
+# # for K, find things with no in-homs
+# not_in_image(ijklm_dat, :K)
+
+# # L
+# not_in_image(ijklm_dat, :L)
+
+# # M
+# not_in_image(ijklm_dat, :M)
