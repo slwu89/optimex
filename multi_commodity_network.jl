978dbdecc9081aedbdbeb43cd4e0c13b2e82ebd7
slwu89 <slwu89@users.noreply.github.com>
2024-09-15 16:49:06 -0700

    reasonable schema for multi commodity network

--- multi_commodity_network.jl
+++ multi_commodity_network.jl
@@ -1,10 +1,9 @@
 using Catlab
 using JuMP
-import DataFrames
+using DataFrames
 import HiGHS
 import SQLite
 import SQLite.DBInterface
-
 
 filename = joinpath(@__DIR__, "commodity_nz.db");
 db = SQLite.DB(filename)
@@ -23,26 +22,69 @@ df_demand = get_table(db, "demand")
 df_cost = DataFrames.leftjoin(df_shipping, df_products; on = [:product])
 df_cost.flow_cost = df_cost.cost_per_km .* df_cost.distance_km
 
+@present SchMultiCommodity <: SchGraph begin    
+    (ProdVertex,Product,Shipping)::Ob
+    pv_p::Hom(ProdVertex,Product)
+    pv_v::Hom(ProdVertex,V)
+    s_p::Hom(Shipping,Product)
+    s_e::Hom(Shipping,E)
 
+    Label::AttrType
+    vlabel::Attr(V,Label)
+    plabel::Attr(Product,Label)
 
-@present NetworkSch <: SchGraph begin
-    NameType::AttrType
-    NumberType::AttrType
-    VectorType::AttrType
-    VectorJumpType::AttrType # hold Vector{JuMP.VariableRef}
-    # data
-    name::Attr(V,NameType)
-    c_ip::Attr(V,VectorType) # cost to purchase
-    u_ip::Attr(V,VectorType) # supply capacity
-    d_ip::Attr(V,VectorType) # demand capacity
-    c_ijp::Attr(E,VectorType) # shipping cost
-    u_ij::Attr(E,NumberType) # flow capacity
-    # decision variables
-    s_ip::Attr(V,VectorJumpType)
-    x_ijp::Attr(E,VectorJumpType)
+    NumVar::AttrType
+    supplycap::Attr(ProdVertex,NumVar) # supply capacity
+    demand::Attr(ProdVertex,NumVar) # demand
+    purcost::Attr(ProdVertex,NumVar) # purchase cost
+    shipcost::Attr(Shipping,NumVar) # shipment cost
+    flowcap::Attr(E,NumVar) # flow capacity
+
+    DecisionVar::AttrType
+    s::Attr(ProdVertex,DecisionVar) # optimal supply
+    x::Attr(Shipping,DecisionVar) # optimal shipment
 end
 
-@acset_type NetworkData(NetworkSch, index=[:src,:tgt]) <: AbstractGraph
+to_graphviz(SchMultiCommodity, graph_attrs=Dict(:size=>"6",:ratio=>"fill"))
 
-to_graphviz(NetworkSch, graph_attrs=Dict(:dpi=>"72",:size=>"4",:ratio=>"expand"))
+@abstract_acset_type AbstractMultiCommodity <: HasGraph
 
+@acset_type MultiCommodity(SchMultiCommodity, index=[:src,:tgt,:pv_p,:pv_v,:s_p,:s_e]) <: AbstractMultiCommodity
+
+places = unique([df_shipping.origin; df_shipping.destination])
+product = unique(df_shipping.product)
+edges=unique(df_shipping[:, [:origin, :destination]])
+transform!(edges, :origin => ByRow(o -> begin
+    findfirst(o .== places)
+end) => :src)
+transform!(edges, :destination => ByRow(o -> begin
+    findfirst(o .== places)
+end) => :tgt)
+
+capacity=30
+
+multinet = @acset MultiCommodity{String,Float64,JuMP.VariableRef} begin
+    V=length(places)
+    vlabel=places
+    Product=length(product)
+    plabel=product
+    E=nrow(edges)
+    src=edges.src
+    tgt=edges.tgt
+    ux=capacity
+end
+
+# add Product X Vertex
+for (v,p) in Iterators.product(places,product)
+    supply_ix = findfirst(df_supply.origin .== v .&& df_supply.product .== p)
+    us = if !isnothing(supply_ix)
+        df_supply[supply_ix, :capacity]
+    else
+        0.0
+    end
+    add_part!(
+        multinet, :ProdVertex, 
+        pv_v=only(incident(multinet, v, :vlabel)), 
+        pv_p=only(incident(multinet, p, :plabel))
+    )
+end
